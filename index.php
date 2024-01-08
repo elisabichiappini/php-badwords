2d6541227253b6ff1844aa02d09fc7fd791fe3a9
elisabichiappini <elisabichiappini@gmail.com>
2024-01-08 21:30:53 +0100

    feat: layout creating

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Badwords</title>
     <!--file css-->
-    <link style="stylesheet" href="./style.css">
+    <link rel="stylesheet" href="./style.css">
     <!--/file css-->
     <!--cdn css bootstrap-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
@@ -14,28 +14,28 @@
 <body>
     <!--intestazione-->
     <header>
-        <h1>Censugrafo</h1>
+        <h1 class="display-1 fw-bold">Censugrafo</h1>
     </header>
     <!--/intestazione-->
     <!--spazio form-->
-    <div class=container>
+    <div class="container">
         <!--form-->
         <form action="response.php" methods="GET">
             <!--input inserire parola-->
             <div class="input-group input-group-sm mb-3">
                 <span class="input-group-text" id="inputGroup-sizing-sm">Word</span>
                 <label for="censura"></label>
-                <input type="text" class="form-control" id="censura" placeholder="parola da censurare" name="censura">
+                <input type="text" class="form-control p-2" id="censura" placeholder="Censurala" name="censura">
             </div>
             <!--/input inserire parola-->
             <!--input inserire paragrafo-->
             <div class="mb-3">
                 <label for="paragrafo" class="form-label"></label>
-                <textarea class="form-control" id="paragrafo" rows="3" placeholder="inserisci paragrafo" name="paragrafo"></textarea>
+                <textarea class="form-control" id="paragrafo" rows="20" placeholder="Il testo da censurare" name="paragrafo" class="p-2"></textarea>
             </div>
             <!--/input inserire paragrafo-->
             <!--inviare il form-->
-            <button type="submit" class="btn btn-primary">Submit</button>
+            <button type="submit" class="btn btn-outline-dark">Submit</button>
             <!--/inviare il form-->
         </form>
         <!--/form-->
